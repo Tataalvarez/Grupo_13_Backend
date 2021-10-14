3cb9a9f0e58bd30b91e91def536d29c99bbc64f9
Tataalvarez <tatianaalvarezmartinez@gmail.com>
2021-10-14 00:24:27 -0500

    Configuración mongo

---
+++ config/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+    dbStringConnect: "mongodb+srv://Grupo_13_Backend:grupo13@cluster0.tuzmh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+}
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express();
+const moongose = require('moongose');
+const dbConfig = require('./config/db');
 
 const port = 3001;
 
