8e85cf1cfc90bcdefc5971bcbbb099967a0ecb46
Tataalvarez <tatianaalvarezmartinez@gmail.com>
2021-10-13 23:36:15 -0500

    Configuración productos

---
+++ controllers/products.controller.js
@@ -0,0 +1,20 @@
+const getProducts = (req, res) => {
+
+}
+
+const createProducts = (req, res) => {
+
+}
+
+const updateProducts = (req, res) => {
+
+}
+
+const deleteProducts = (req, res) => {
+
+}
+
+module.exports.getProducts= getProducts;
+module.exports.createProducts = createProducts;
+module.exports.updateProducts = updateProducts;
+module.exports.deleteProducts = deleteProducts;
--- routes/products.router.js
+++ routes/products.router.js
@@ -1,20 +1,13 @@
 const { Router } = require('express');
 const router = Router();
+const {productsController} = require('../controllers');
 
-router.get('/', (req, res) => {
-    res.json({ msg: "todos los productos" });
-})
+router.get('/', productsController.getProducts);
 
-router.post('/', (req, res) => {
-    res.json({ msg: "producto insertado" });
-})
+router.post('/', productsController.createProducts);
 
-router.put('/', (req, res) => {
-    res.json({ msg: "producto actualizado" });
-})
+router.put('/', productsController.updateProducts);
 
-router.delete('/', (req, res) => {
-    res.json({ msg: "producto eliminado" });
-})
+router.delete('/', productsController.deleteProducts);
 
 module.exports = router;
