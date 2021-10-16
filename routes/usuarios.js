623b3b571b172707778116f5de8b01d19e59e16f
richmon43 <richmon43@gmail.com>
2021-10-16 12:12:38 -0500

    agregando validaciones a datos de usuario

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -2,8 +2,13 @@
 const express = require('express');
 const router = express.Router();
 const usuarioController = require('../controllers/usuarioController');
+const { check } = require('express-validator');
 
 // Crea un usuario en /api/usuarios
-router.post('/', usuarioController.crearUsuario);
+router.post('/', [
+  check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+  check('email', 'Agrega un email válido').isEmail(),
+  check('password', 'La contraseña debe tener mínimo 6 caracteres').isLength({ min: 6 })
+], usuarioController.crearUsuario);
 
 module.exports = router;
