17ebed93eec4f62c93a8694a42556f3f3f7640dc
Richar Mora <71612213+richmon43@users.noreply.github.com>
2021-10-19 01:18:42 -0500

    update db.js
    Implementando variables de entorno

--- config/db.js
+++ config/db.js
@@ -1,3 +1,11 @@
-module.exports = {
-    dbStringConnect: "mongodb+srv://Grupo_13_Backend:grupo13@cluster0.tuzmh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-}
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const uri = `mongodb+srv://${process.env.user}:${process.env.pass}@${process.env.host}`;
+
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("DB conectada"))
+  .catch((e) => console.log("error de conexión", e));
+
+module.exports = mongoose;
